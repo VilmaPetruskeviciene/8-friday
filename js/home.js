daaf325d7e98e8c3378ab4b150ef2233b905f44f
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-07-26 10:59:16 +0300

    hello js

--- js/home.js
+++ js/home.js
@@ -7,3 +7,14 @@ const sav = ['Pirmadienis', 'Antradienis', 'Trečiadienis', 'Ketvirtadienis', 'P
 let today2 = new Date();
 let date = (sav[today2.getDay()])+', '+(men[today2.getMonth()])+' '+today2.getDate()+', '+today2.getFullYear();
 document.getElementById('date').innerHTML = date;
+
+//const day = ['Good mornig', 'Good afternoon', 'Good evening'];
+function myday(time) {
+    if (time >= '00:00' && time <= '11:59') {
+        return 'Good mornig, '+'<br>'+'Vilma';
+    } else if (time >= '12:00' && time <= '16:59') {
+        return 'Good afternoon, '+'<br>'+'Vilma';
+    } 
+        return 'Good evening, '+'<br>'+'Vilma';
+}
+document.getElementById('hello').innerHTML = myday(time);
