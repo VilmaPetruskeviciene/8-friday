6d6f4ef6bffd049127ff6d6a7a6cd1502adda349
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-07-27 09:05:45 +0300

    js tais

--- js/home.js
+++ js/home.js
@@ -1,14 +1,21 @@
-let today = new Date();
-let time = today.getHours() + ":" + today.getMinutes();
-document.getElementById('time').innerHTML = time;
+function myTime() {
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes();
+    document.getElementById('time').innerHTML = time;
+    setTimeout(myTime, 1000);
+}
+myTime();
 
 const men = ["Sausio","Vasario","Kovo","Balandžio","Gegužės","Birželio","Liepos","Rugpjūčio","Rugsėjo","Spalio","Lapkričio","Gruodžio"];
 const sav = ['Pirmadienis', 'Antradienis', 'Trečiadienis', 'Ketvirtadienis', 'Penktadienis', 'Šeštadienis', 'Sekmadienis'];
-let today2 = new Date();
-let date = (sav[today2.getDay()])+', '+(men[today2.getMonth()])+' '+today2.getDate()+', '+today2.getFullYear();
-document.getElementById('date').innerHTML = date;
+function myDate() {
+    let today2 = new Date();
+    let date = sav[today2.getDay()]+', '+men[today2.getMonth()]+' '+today2.getDate()+', '+today2.getFullYear();
+    document.getElementById('date').innerHTML = date;
+    setTimeout(myDate, 1000);
+}
+myDate();
 
-//const day = ['Good mornig', 'Good afternoon', 'Good evening'];
 function myday(time) {
     if (time >= '00:00' && time <= '11:59') {
         return 'Good mornig, '+'<br>'+'Vilma';
