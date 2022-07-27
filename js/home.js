445fd0c5501c8b03cabc547e78125e655314766b
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-07-27 09:41:53 +0300

    js

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
-function myTime() {
-    let today = new Date();
-    let time = today.getHours() + ":" + today.getMinutes();
+let today = new Date();
+let time = today.getHours() + ":" + today.getMinutes();
+
+function myTime() {  
     document.getElementById('time').innerHTML = time;
     setTimeout(myTime, 1000);
 }
@@ -9,17 +10,16 @@ myTime();
 const men = ["Sausio","Vasario","Kovo","Balandžio","Gegužės","Birželio","Liepos","Rugpjūčio","Rugsėjo","Spalio","Lapkričio","Gruodžio"];
 const sav = ['Pirmadienis', 'Antradienis', 'Trečiadienis', 'Ketvirtadienis', 'Penktadienis', 'Šeštadienis', 'Sekmadienis'];
 function myDate() {
-    let today2 = new Date();
-    let date = sav[today2.getDay()]+', '+men[today2.getMonth()]+' '+today2.getDate()+', '+today2.getFullYear();
+    let date = sav[today.getDay()]+', '+men[today.getMonth()]+' '+today.getDate()+', '+today.getFullYear();
     document.getElementById('date').innerHTML = date;
     setTimeout(myDate, 1000);
 }
 myDate();
 
 function myday(time) {
-    if (time >= '00:00' && time <= '11:59') {
+    if (time >= 00 && time < 12) {
         return 'Good mornig, '+'<br>'+'Vilma';
-    } else if (time >= '12:00' && time <= '16:59') {
+    } else if (time >= 12 && time < 17) {
         return 'Good afternoon, '+'<br>'+'Vilma';
     } 
         return 'Good evening, '+'<br>'+'Vilma';
