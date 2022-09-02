39590af5c125a0f0b93fee600eb88839a8635a6d
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-09-02 14:21:53 +0300

    html

--- js/time.js
+++ js/time.js
@@ -1,8 +1,14 @@
 function myTime() {
   let today = new Date();
-  let time = today.getHours() + ":" + today.getMinutes();  
-  document.getElementById('time').innerHTML = time;
-  setTimeout(myTime, 1000);
+  if (today.getHours() < 10 && today.getMinutes() < 10) {
+    let time = "0" + today.getHours() + ":" + "0" + today.getMinutes();  
+    document.getElementById('time').innerHTML = time;
+    setTimeout(myTime, 1000);
+  } else {
+    let time = today.getHours() + ":" + today.getMinutes();  
+    document.getElementById('time').innerHTML = time;
+    setTimeout(myTime, 1000);
+  }
 }
 
 myTime();
